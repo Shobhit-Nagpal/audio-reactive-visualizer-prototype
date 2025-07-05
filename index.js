0297827f851bad573adb4ac49fd4843097e3d0d2
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2025-07-05 14:15:35 +0530

    take in audio as input and log visualization

---
+++ index.js
@@ -0,0 +1,57 @@
+console.log("Sup");
+
+const recordBtn = document.getElementById("record-btn");
+let ctx = null;
+
+recordBtn.addEventListener("click", async () => {
+  try {
+    ctx = new AudioContext();
+    const stream = await accessMicrophone();
+    if (!ctx) {
+      throw new Error("Context is not defined");
+    }
+    const source = ctx.createMediaStreamSource(stream);
+    const analyser = createAnalyser(ctx);
+    analyser.fftSize = 1024;
+
+    source.connect(analyser);
+
+    const buffLength = analyser.frequencyBinCount;
+    const dataArray = new Uint8Array(buffLength);
+
+    function visualize() {
+      analyser.getByteFrequencyData(dataArray);
+      // Now dataArray contains your frequency data (0-255 values)
+      // Each index represents a frequency bin
+      console.log(dataArray); // This is your "view" into the audio
+      requestAnimationFrame(visualize);
+    }
+    visualize(dataArray, analyser);
+  } catch (err) {
+    if (err instanceof DOMException) {
+      console.error(err);
+      alert("YOU ARE NGMI");
+      return;
+    }
+    console.error(err);
+  }
+});
+
+/**
+ * Access users' microphone
+ * @returns {Promise<MediaStream>} mediaStream
+ */
+async function accessMicrophone() {
+  return await navigator.mediaDevices.getUserMedia({
+    audio: true,
+  });
+}
+
+/**
+ * Returns an AnalyserNode
+ * @param {AudioContext} ctx
+ * @returns {AnalyserNode} analyserNode
+ */
+function createAnalyser(ctx) {
+  return ctx.createAnalyser();
+}
