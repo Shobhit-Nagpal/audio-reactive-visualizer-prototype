fc6446ddcc1b679570cf3386ab4f052f10289cc4
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2025-07-15 21:22:35 +0530

    audio prototype v2

--- index.js
+++ index.js
@@ -1,212 +1,159 @@
-console.log("Sup");
+const startBtn = document.getElementById("start-btn");
+const canvas = document.getElementById("canvas");
+/**
+ * @type {AudioContext}
+ */
+let audioContext = null;
 
-const recordBtn = document.getElementById("record-btn");
-const leds = document.querySelector("#leds");
-let ctx = null;
+/**
+ * @type {CanvasRenderingContext2D}
+ */
+const canvasContext = canvas.getContext("2d");
 
-document.body.onload = () => {
-  for (let i = 0; i < 300; i++) {
-    const led = document.createElement("div");
-    led.id = `led-${i + 1}`;
-    led.classList.add("led");
-    leds.appendChild(led);
-  }
-};
-
-recordBtn.addEventListener("click", async () => {
-  try {
-    const stream = await accessMicrophone();
-    if (!ctx) {
-      ctx = new AudioContext();
-    }
-    const source = ctx.createMediaStreamSource(stream);
-    const analyser = createAnalyser(ctx);
-    analyser.fftSize = 1024;
-
-    source.connect(analyser);
-
-    const buffLength = analyser.fftSize;
-    const amplitudeDataArray = new Uint8Array(buffLength);
-    const frequencyDataArray = new Uint8Array(buffLength);
-
-    function visualize() {
-      analyser.getByteTimeDomainData(amplitudeDataArray);
-      analyser.getByteFrequencyData(frequencyDataArray);
-
-      const bassRange = frequencyDataArray.slice(0, 85); // Low frequencies
-      const midRange = frequencyDataArray.slice(85, 341); // Mid frequencies
-      const trebRange = frequencyDataArray.slice(341, 512); // High frequencies
-
-      const transformedArray = amplitudeDataArray.map((x) => x - 128);
-      const amplitude = rootMeanSquare(transformedArray);
-      const bassLevel =
-        bassRange.reduce((sum, val) => sum + val, 0) / bassRange.length;
-      const midLevel =
-        midRange.reduce((sum, val) => sum + val, 0) / midRange.length;
-      const trebLevel =
-        trebRange.reduce((sum, val) => sum + val, 0) / trebRange.length;
-
-      const freqRMS = rootMeanSquare([bassLevel, midLevel, trebLevel]);
-
-      updateAmplitudeLEDs_v3(amplitude);
-      //updateFreqLEDs(freqRMS);
-      updateBassLEDs(bassLevel, 93);
-      requestAnimationFrame(visualize);
-    }
-    visualize(amplitudeDataArray, analyser);
-  } catch (err) {
-    if (err instanceof DOMException) {
-      console.error(err);
-      alert("YOU ARE NGMI");
-      return;
-    }
-    console.error(err);
-  }
+startBtn.addEventListener("click", () => {
+  init();
 });
 
+async function init() {
+  audioContext = new AudioContext();
+
+  const source = await getAudioSource();
+
+  if (!source) {
+    alert("Please give access to browser to use mic");
+    return;
+  }
+
+  const analyser = audioContext.createAnalyser();
+  source.connect(analyser);
+
+  analyser.fftSize = 1024;
+
+  const bufferLength = analyser.frequencyBinCount; // fftSize / 2
+  const freqDataArray = new Uint8Array(bufferLength);
+  const ampDataArray = new Uint8Array(bufferLength);
+  const range = createFrequencyRanges(audioContext.sampleRate, analyser.fftSize)
+
+  function draw() {
+    // Do the visual part here
+    analyser.getByteFrequencyData(freqDataArray);
+    analyser.getByteTimeDomainData(ampDataArray);
+
+    const frequencies = extractFrequencyFeatures(freqDataArray, range);
+    const amplitude = extractAmplitude(ampDataArray);
+
+    console.log("f:", frequencies);
+    console.log("a:", amplitude);
+
+    requestAnimationFrame(draw);
+  }
+
+  draw();
+}
+
 /**
- * Access users' microphone
- * @returns {Promise<MediaStream>} mediaStream
+ * Gets audio source by asking user about device perms
+ * @returns {MediaStreamAudioSourceNode | null}
  */
-async function accessMicrophone() {
-  return await navigator.mediaDevices.getUserMedia({
-    audio: true,
-  });
+async function getAudioSource() {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+    });
+
+    return audioContext.createMediaStreamSource(stream);
+  } catch (err) {
+    console.error("Bruh moment", err);
+    return null;
+  }
 }
 
 /**
- * Returns an AnalyserNode
- * @param {AudioContext} ctx
- * @returns {AnalyserNode} analyserNode
+ * @param {Uint8Array} dataArray - represents frequency domain data
+ * @param {object} range - object containing upper and lower values on frequency ranges
+ * @returns {bass: number[], lowMids: number[], mids: number[], highMids: number[], highs: number[]}
  */
-function createAnalyser(ctx) {
-  return ctx.createAnalyser();
+function extractFrequencyFeatures(dataArray, range) {
+  console.log("length:", dataArray.length);
+  const bass = dataArray.slice(range.BASS.LOWER, range.BASS.UPPER);
+  const lowMids = dataArray.slice(
+    range.LOW_MIDS.LOWER,
+    range.LOW_MIDS.UPPER,
+  );
+  const mids = dataArray.slice(range.MIDS.LOWER, range.MIDS.UPPER);
+  const highMids = dataArray.slice(
+    range.HIGH_MIDS.LOWER,
+    range.HIGH_MIDS.UPPER,
+  );
+  const highs = dataArray.slice(range.HIGHS.LOWER, range.HIGHS.UPPER);
+
+  return {
+    bass,
+    lowMids,
+    mids,
+    highMids,
+    highs,
+  };
 }
 
 /**
- * Returns the RMS
- * @param {Uint8Array<ArrayBuffer>} buffer
- * @returns {number}
+ * Performs RMS on time-domain data to get amplitude
+ * @param {Uint8Array} dataArray - represents time-domain data
+ * @returns {number} amplitude
  */
-function rootMeanSquare(buffer) {
-  const squared = buffer.map((x) => x * x);
-  const sum = squared.reduce((acc, val) => acc + val, 0);
-  const mean = sum / buffer.length;
-  const rms = Math.sqrt(mean);
-  return rms;
+function extractAmplitude(dataArray) {
+  let sum = 0;
+  for (let i = 0; i < dataArray.length; i++) {
+    const val = (dataArray[i] - 128) / 128;
+    const squared = val * val;
+    sum += squared;
+  }
+
+  const amplitude = Math.sqrt(sum / dataArray.length);
+
+  return amplitude;
 }
 
-/*
- *
- *
- * LED Module
- *
- *
+/**
+ * Tells number of bins for a frequency
+ * @param {number} frequency
+ * @param {number} sampleRate
+ * @param {number} fftSize
+ * @return {number} bin
  */
-function updateAmplitudeLEDs_v1(amplitude) {
-  if (amplitude > 2.5) {
-    Array.from(document.querySelectorAll(".led"))
-      .filter(
-        (led) =>
-          parseInt(led.id.split("-")[1]) >= 100 &&
-          parseInt(led.id.split("-")[1]) <= 300,
-      )
-      .forEach((led) => led.classList.remove("low"));
-    Array.from(document.querySelectorAll(".led"))
-      .filter(
-        (led) =>
-          parseInt(led.id.split("-")[1]) >= 100 &&
-          parseInt(led.id.split("-")[1]) <= 300,
-      )
-      .forEach((led) => led.classList.add("high"));
-  } else {
-    document
-      .querySelectorAll(".led")
-      .forEach((led) => led.classList.remove("high"));
-    document
-      .querySelectorAll(".led")
-      .forEach((led) => led.classList.add("low"));
-  }
-}
-function updateAmplitudeLEDs_v2(amplitude) {
-  const amplitudeLEDs = document.querySelectorAll(".led"); // LEDs 1-100 and 301-400
-
-  if (amplitude > 2.5) {
-    amplitudeLEDs.forEach((led) => {
-      led.classList.remove("low");
-      led.classList.add("high");
-    });
-  } else {
-    amplitudeLEDs.forEach((led) => {
-      led.classList.remove("high");
-      led.classList.add("low");
-    });
-  }
+function frequencyToBin(frequency, sampleRate, fftSize) {
+  const nyquist = sampleRate / 2;
+  const binWidth = nyquist / (fftSize / 2);
+  const bin = Math.floor(frequency / binWidth);
+  return bin;
 }
 
-function updateAmplitudeLEDs_v3(amplitude) {
-  const amplitudeLEDs = document.querySelectorAll(".led"); // LEDs 1-100 and 301-400
-
-  if (amplitude > 2.5) {
-    amplitudeLEDs.forEach((led) => {
-      led.style.opacity = 0.5;
-    });
-  } else {
-    amplitudeLEDs.forEach((led) => {
-      led.style.opacity = 1;
-    });
-  }
-}
-
-function updateBassLEDs(bassLevel, level) {
-  const bassLEDs = document.querySelectorAll(".led"); // LEDs 101-300
-
-  const half1 = Array.from(bassLEDs).filter(led => Number(led.id.split('-')[1]) % 2 === 0);
-  const half2 = Array.from(bassLEDs).filter(led => Number(led.id.split('-')[1]) % 2 !== 0);
-
-  console.log(bassLevel);
-
-  if (bassLevel > level) {
-    // Adjust threshold as needed
-    half1.forEach((led) => {
-      led.classList.remove("low");
-      led.classList.add("high");
-    });
-    half2.forEach((led) => {
-      led.classList.add("low");
-      led.classList.remove("high");
-    });
-  } else {
-    half2.forEach((led) => {
-      led.classList.remove("low");
-      led.classList.add("high");
-    });
-    half1.forEach((led) => {
-      led.classList.add("low");
-      led.classList.remove("high");
-    });
-  }
-}
-
-function updateFreqLEDs(freqRMS) {
-  const bassLEDs = document.querySelectorAll(".led"); // LEDs 101-300
-
-  if (freqRMS > 69) {
-    // Adjust threshold as needed
-    bassLEDs.forEach((led) => {
-      led.classList.remove("low");
-      led.classList.add("high");
-    });
-  } else {
-    bassLEDs.forEach((led) => {
-      led.classList.remove("high");
-      led.classList.add("low");
-    });
-  }
-}
-
-/*
- * new Array(1024);
- * [0...1023]
- * 0 <= freq <= 255
+/**
+ * @param {number} sampleRate
+ * @param {number} fftSize
+ * @return {object} - Frequency ranges object with upper and lower values 
  */
+function createFrequencyRanges(sampleRate, fftSize) {
+  return {
+    BASS: {
+      LOWER: frequencyToBin(80, sampleRate, fftSize),
+      UPPER: frequencyToBin(250, sampleRate, fftSize),
+    },
+    LOW_MIDS: {
+      LOWER: frequencyToBin(250, sampleRate, fftSize),
+      UPPER: frequencyToBin(800, sampleRate, fftSize),
+    },
+    MIDS: {
+      LOWER: frequencyToBin(800, sampleRate, fftSize),
+      UPPER: frequencyToBin(3_000, sampleRate, fftSize),
+    },
+    HIGH_MIDS: {
+      LOWER: frequencyToBin(3_000, sampleRate, fftSize),
+      UPPER: frequencyToBin(8_000, sampleRate, fftSize),
+    },
+    HIGHS: {
+      LOWER: frequencyToBin(8_000, sampleRate, fftSize),
+      UPPER: frequencyToBin(15_000, sampleRate, fftSize),
+    },
+  };
+}
